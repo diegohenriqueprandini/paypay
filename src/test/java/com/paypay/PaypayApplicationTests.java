8943741f09efb8545098b0d0ceb790e21856c8e5
diego.prandini <contato.diego.prandini@gmail.com>
2023-02-19 10:52:08 -0300

    initial commit

---
+++ src/test/java/com/paypay/PaypayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
