ef871c305d0771b10585d1afbd4ffcff1915cf22
diego.prandini <contato.diego.prandini@gmail.com>
2023-02-20 01:47:19 -0300

    Clean arch

--- src/test/java/com/paypay/PaypayApplicationTests.java
+++ src/test/java/com/paypay/PaypayApplicationTests.java
@@ -2,12 +2,13 @@ package com.paypay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("memory")
 class PaypayApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
