21329470f7e1734842d3e4797e4abf749fd81212
diego.prandini <contato.diego.prandini@gmail.com>
2023-02-21 16:30:35 -0300

    clean arch

--- src/main/java/com/paypay/infra/config/ApiConfig.java
+++ src/main/java/com/paypay/infra/config/ApiConfig.java
@@ -1,14 +1,16 @@
 package com.paypay.infra.config;
 
+import com.paypay.infra.properties.ApplicationProperties;
 import com.paypay.utils.JsonUtils;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class ApiConfig {
 
     @Bean
@@ -17,12 +19,11 @@ public class ApiConfig {
     }
 
     @Bean
-    public OpenAPI customOpenApi() {
-        return new OpenAPI().info(new Info()
-                .title("PAYPAY")
-                .version("v1")
-                .description("Api for Paypay")
-                .termsOfService("Terms")
-                .license(new License().name("APACHE").url("URL")));
+    public OpenAPI customOpenApi(ApplicationProperties applicationProperties) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(applicationProperties.getTitle())
+                        .version(applicationProperties.getVersion())
+                        .description("Paypay events api"));
     }
 }
